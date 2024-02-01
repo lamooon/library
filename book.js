f2c3aa0734e5e40082d2d6d99777e66cb7d5d32b
lamooon <111581657+lamooon@users.noreply.github.com>
2024-02-01 12:56:27 +0900

    Completed assignment except few CSS

--- book.js
+++ book.js
@@ -3,6 +3,7 @@ const addBookBtn = document.querySelector('.add-book');
 const libraryDiv = document.querySelector('.my-library');
 const dialog = document.getElementById('adding-book');
 const addBtn = document.querySelector('.submit-book');
+const getForm = document.querySelector('.add-form');
 // book constructor
 function Book(title, author, pages, readStatus) {
     this.title = title;
@@ -29,14 +30,13 @@ function displayLibrary(book) {
     let title = document.createElement('div');
     let author = document.createElement('div');
     let pages = document.createElement('div');
-    let status = document.createElement('div');
+    let status = document.createElement('button');
     let removeButton = document.createElement('button');
 
 
     title.classList.add('title');
     author.classList.add('author');
     pages.classList.add('pages');
-    status.classList.add('status');
     removeButton.classList.add('remove-book');
 
     title.textContent = "Title: " + book.title;
@@ -44,23 +44,45 @@ function displayLibrary(book) {
     pages.textContent = book.pages + " pages";
     removeButton.textContent = "Remove";
 
-
+    // status
     if (book.readStatus === false) {
-        status.textContent = "Status: Not Read.";
-    } else {
-        status.textContent = "Read";
+        status.textContent = "Not Read";
+        status.classList.add("not-read");
     }
+    else {
+        status.textContent = "Read";
+        status.classList.add("read-book");
+    }
+
+    status.addEventListener('click', () => {
+        if (status.textContent.includes("Not Read")) {
+            status.textContent = "Read";
+            status.classList.remove("not-read");
+            status.classList.add("read-book");
+            myLibrary.find(b => b.title === book.title).readStatus = true;
+        } else {
+            status.textContent = "Not Read";
+            status.classList.remove("read-book");
+            status.classList.add("not-read");
+            myLibrary.find(b => b.title === book.title).readStatus = false;
+        }
+    })
+
+
+
 
     bookDiv.append(title, author, pages, status, removeButton);
     libraryDiv.appendChild(bookDiv);
 
     removeButton.onclick = function() {
         // myLibrary = myLibrary.filter(book => book.title !== book.title);
-        let index = Array.prototype.findIndex.call(myLibrary, (x) => (x.title === book.title && x.pages === book.pages));
+        let index = Array.prototype.findIndex.call(myLibrary, (x) => (x.title === book.title && x.pages === book.pages && (x.author === book.author)));
         console.log("index is: " + index);
         myLibrary.splice(index, 1);
         bookDiv.parentNode.removeChild(bookDiv);
     }
+
+
 }
 
 addBookBtn.addEventListener('click', () => {
@@ -72,15 +94,12 @@ addBtn.addEventListener("click", (event) => {
     const title = document.getElementById('title');
     const author = document.getElementById('author');
     const pages = document.getElementById('pages');
-    const read = document.getElementById('read');
+    const read = document.getElementById('read-check');
 
     addBookToLibrary(title.value, author.value, pages.value, read.checked);
 
     // resetting the values after closing dialog
-    title.value = '';
-    author.value = '';
-    pages.value = '';
-    read.checked = false;
+    getForm.reset();
     dialog.close();
 })
 
